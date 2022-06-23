794944d6aa8d0a60dce5ffc4ef91f9803d474581
ADMIN <ADMIN@DESKTOP-DKDQVD8>
2022-06-23 17:42:19 +0530

    Moved to staging area

---
+++ src/abcdef/StashGitHub.java
@@ -0,0 +1,14 @@
+package abcdef;
+
+public class StashGitHub {
+
+	public static void main(String[] args) {
+		int a=55;
+		int b=56;
+		int c=a+b;
+		System.out.println(c);
+System.out.println("my name is Ramesh");
+System.out.println("I'm doing the conflict concept");
+	}
+
+}
